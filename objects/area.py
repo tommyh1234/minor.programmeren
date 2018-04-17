a3a7e8900b2d693402de8363eb249cad8426f641
Jos Vlaar <github@josvlaar.nl>
2018-04-17 13:59:18 +0200

    Moved validation logic from house to area

--- objects/area.py
+++ objects/area.py
@@ -9,7 +9,7 @@ class Area:
     def place_house(self, house, x, y):
         house.x = x
         house.y = y
-        if house.check_validity(self):
+        if self.check_validity(house):
             for i in range(x, x + house.width):
                 for j in range(y, y + house.height):
                     self.grid[i][j] = house
@@ -17,6 +17,19 @@ class Area:
         else:
             house.x = None
             house.y = None
+
+    def check_validity(self, house):
+        endX = house.x + house.width
+        endY = house.y + house.height
+
+        for i in range(house.x, endX):
+            for j in range(house.y, endY):
+                # if there's something that is not this house
+                if (self.grid[i][j] is not None
+                        and self.grid[i][j] is not house):
+                    return False
+
+        return True
 
     def check_house_balance(self):
         return 1
--- objects/house.py
+++ objects/house.py
@@ -1,17 +1,22 @@
 class House:
-    x = 0
-    y = 0
+    x = None
+    y = None
     height = 0
     width = 0
     minimumSpace = 0
     priceIncrease = 0
+    space = 0
     value = 0
+
+    def __init__(self, space):
+        self.space = space
+        self.width = self.width + space * 2
+        self.height = self.height + space * 2
 
     def surface(self):
         return self.width * self.height
 
-    def check_validity(self, map):
-        return True
-
-    def check_price(self, map):
-        return 0
+    def get_price(self, area):
+        price = self.value * (1 + (self.space - self.minimumSpace)
+                              / 2 * self.priceIncrease)
+        return price
