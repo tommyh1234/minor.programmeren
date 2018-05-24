0b4d2918c45c7846044e6dcdf263deafe8cdea37
Jos Vlaar <github@josvlaar.nl>
2018-05-24 12:52:04 +0200

    Improved things

--- application.py
+++ application.py
@@ -12,31 +12,24 @@ from datahelper import DataHelper
 
 def main():
     print("WELCOME TO AMSTELHAEGE")
-    gridChoice = int(input('Do you want to load in a grid or start from scratch?\
-                            1: Load a grid\
-                            2: Start from scratch'))
-    algorithmChoice = int(input('What algorithm do you want to run?\
-                                1: Random \
-                                2: SpeedRandom\
-                                3: HillClimbing\
-                                4: Simmulated Annealing'))
-    visualizerChoice = int(input('What visualizer do you want?\
-                                 1: Normal visualizer\
-                                 2: Bulk visualizer'))
+    gridChoice = int(input('Do you want to load in a grid or start from scratch?\n\
+ 1: Load a grid\n 2: Start from scratch\n'))
+    algorithmChoice = int(input('What algorithm do you want to run?\n \
+ 1: Random \n 2: SpeedRandom\n 3: HillClimbing\n 4: Simmulated Annealing\n'))
+    visualizerChoice = int(input('What visualizer do you want?\n \
+ 1: Normal visualizer\n 2: Bulk visualizer\n'))
     isEmpty = True
     fhAmount = 0
     bAmount = 0
     mAmount = 0
     if gridChoice == 1:
-        fileName = str(input('Please give me a file name: '))
+        fileName = str(input('Please give me a file name: \n'))
         area = DataHelper(fileName).getArea()
         isEmpty = False
     else:
         area = Area()
-        amountChoice = int(input('How many houses do you want? \
-                                  1: 20\
-                                  2: 40\
-                                  3: 60'))
+        amountChoice = int(input('How many houses do you want? \n\
+ 1: 20\n 2: 40\n 3: 60\n'))
         if amountChoice == 1:
             fhAmount = 12
             bAmount = 5
@@ -66,7 +59,7 @@ def main():
     if visualizerChoice == 1:
         visualizer = Visualizer(area, algorithm)
     elif visualizerChoice == 2:
-        runs = int(input('How many runs do you want to do? '))
+        runs = int(input('How many runs do you want to do? \n'))
         visualizer = BulkVisualizer(area, algorithm, runs)
 
     visualizer.on_execute()
